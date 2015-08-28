b17da2fdbea94001764a7dfb6c89413d7e036e5e
loyuehei <logap@me.com>
2015-08-28 11:24:13 +0800

    Task distribution procedure &  factorial calculation

--- src/main/scala/TaskMaster.scala
+++ src/main/scala/TaskMaster.scala
@@ -1,38 +1,80 @@
-import akka.actor.{Terminated, PoisonPill, Props, Actor}
+import akka.actor._
 import scala.collection.mutable
 import scala.concurrent.duration._
+import scala.util.control.TailCalls.TailRec
 
 /**
  * Created by Gary Lo on 27/8/15.
  */
-class TaskMaster extends Actor {
+class TaskMaster extends Actor with ActorLogging {
   private val q = new mutable.Queue[Task]()
   override def receive: Receive = {
     case JobDescriptions(size, numOfWorker) =>
       // initialize the Task according to the description
       for(i <- 1 to size){
-        q.enqueue(Task(i, i seconds))
+        q.enqueue(Task(id = i, func = factorial))
       }
 
       // initialize the Child worker according to the description
       for(i <- 1 to numOfWorker ){
-        val child = context.actorOf(Props[TaskWorker], s"worker_$i")
-        if(q.nonEmpty) child ! q.dequeue()
+        // create Child Actor with id = 00001, 00002.. so on
+        context.actorOf(Props[TaskWorker], f"$i%05d")
       }
 
+      // after init,  start jobDistribution
+      self ! JobDistribution
+
     case JobDone(result) =>
-      if(q.nonEmpty) sender() ! q.dequeue()
-      else {
+      log.info(s"[${sender().path.name}] has finished his job")
+      if(q.isEmpty){
         context.watch(sender())
         sender() ! PoisonPill
       }
 
+    case JobDistribution =>
+      // it is redundant in real-world case
+      val sortedChild = context.children.toList.sortBy(ref => ref.path.name.toInt)
+
+      while(q.nonEmpty) {
+        // sort the children first then distribute the task to them
+        sortedChild.foreach(ref =>
+          ref ! q.dequeue()
+        )
+      }
+      // after distribution, start to work
+      self ! StartToWork
+
+    case StartToWork =>
+      // force child to work
+      context.children.foreach(ref =>
+        ref ! StartToWork
+      )
+
     case Terminated(subject) =>
-      println(s"receive Terminated $subject")
+      log.info(s"receive Terminated $subject")
       if(context.children.isEmpty) context.parent ! PoisonPill
   }
+
+  /**
+   * The factorial task
+   * @param n       size of factorial
+   * @param result  factorial Result
+   * @return
+   */
+  private def factorial(n : Int, result: BigDecimal = BigDecimal(1)): BigDecimal = {
+    if(n > 0) factorial(n-1, result * BigDecimal(n))
+    else result
+  }
+
 }
-case class Task(id: Int, duration: Duration)
+
+// Job description from system
 case class JobDescriptions(taskSize: Int, numOfWorker: Int)
-case class JobDone(result:Option[Int])
-case object Terminal
+
+// Task definition
+case class Task(id: Int, func: (Int, BigDecimal) => BigDecimal, result: Option[BigDecimal] = None)
+
+// For child case
+case class JobDone(result:Option[BigDecimal])
+case object JobDistribution
+case object StartToWork
--- src/main/scala/TaskWorker.scala
+++ src/main/scala/TaskWorker.scala
@@ -1,22 +1,69 @@
-import akka.actor.{Actor}
+import akka.actor.{ActorLogging, Actor}
+
+import scala.collection.mutable
 
 /**
  * Created by Gary Lo on 27/8/15.
  */
-class TaskWorker extends Actor{
+class TaskWorker extends Actor with ActorLogging{
+
+  private val q = mutable.Queue[Task]()
 
   override def receive: Receive = {
-    case Task(id,duration) =>
 
-      println(s"[${self.path.name}] Doing job $id")
+    case task @ Task(_,_,_) =>
+      q.enqueue(task)
 
-      // TODO: really do a blocking job instead of sleep
-      Thread.sleep(duration.toMillis)
+    case StartToWork =>
 
-      println(s"[${self.path.name}] Job Done! Time used = $duration")
+      if(q.nonEmpty){
+        val task = q.dequeue()
 
-      sender() ! JobDone(Some(id))
+        val result = doTask(task)
 
+        self ! GoNextTask(
+          previousTask = Task(id = task.id, func = task.func, result = Some(result)),
+          nextTaskOpt = if(q.nonEmpty) Some(q.dequeue()) else None
+        )
+      }
+      else{
+        context.parent ! JobDone(None)
+      }
+
+
+    case GoNextTask(pTask: Task, nTask: Option[Task]) =>
+      nTask match {
+        case None =>
+          // Tell the parent you are done
+          context.parent ! JobDone(pTask.result)
+        case Some(task) =>
+          val pResult = pTask.result.getOrElse(BigDecimal(0))
+          val nResult = pResult + doTask(task)
+
+          log.info(s"${pTask.id}! + ${task.id}! = $nResult")
+
+          self ! GoNextTask(
+            previousTask = Task(id = task.id, func = task.func, result = Some(nResult)),
+            nextTaskOpt = if(q.nonEmpty) Some(q.dequeue()) else None
+          )
+      }
   }
 
+  /**
+   * Do the Task Impl
+   * @param task       Task
+   * @return
+   */
+  def doTask(task: Task) = {
+    val startTime = System.nanoTime()
+    log.info(s"Doing Factorial ${task.id}!")
+    val result = task.func(task.id, BigDecimal(1))
+    log.info(s"Time Used = ${(System.nanoTime() - startTime)/1000000.0}ms for ${task.id}! = $result")
+
+    result
+  }
+
+  // generic Go Next Task
+  case class GoNextTask(previousTask: Task, nextTaskOpt: Option[Task])
 }
+
